331d1168bb25e4b5d963c7916b67c9150bea1d1c
Candace Eckels <candace.eckels@gmail.com>
2023-03-06 14:16:11 -0700

    Refactored Museum: recommend_exhibit methods

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Museum
   attr_reader :name, :exhibits, :patrons
 
@@ -7,8 +8,10 @@ class Museum
     @patrons = []
   end
 
-  def recommend_exhibits(patron_interests)
-    patron.interests
+  def recommend_exhibits(patron)
+    @exhibits.map do |exhibit|
+      exhibit if patron.interests.include?(exhibit.name)
+    end.compact.sort_by { |exhibit| exhibit.name }
   end
 
   def add_exhibit(exhibit)
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -4,76 +4,98 @@ require './lib/exhibit'
 require 'pry'
 
 RSpec.describe Museum do
-
-  it "exists" do
-
-    dmns = Museum.new("Denver Museum of Nature and Science")
-
-    expect(dmns).to be_a(Museum)
+  before :each do
+    @dmns = Museum.new("Denver Museum of Nature and Science")
   end
 
-  it "has a name" do
-
-    dmns = Museum.new("Denver Museum of Nature and Science")
-
-    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      expect(@dmns).to be_a(Museum)
+      expect(@dmns.name).to eq("Denver Museum of Nature and Science")
+      expect(@dmns.exhibits).to be_empty
+      expect(@dmns.exhibits).to be_a Array
+      expect(@dmns.patrons).to be_empty
+      expect(@dmns.exhibits).to be_a Array
+    end
   end
 
-  it "has exhibits" do
+  describe '#add_exhibit(exhibit)' do
+    it 'can add exhibits' do
+      gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      imax = Exhibit.new({name: "IMAX",cost: 15})
 
-    dmns = Museum.new("Denver Museum of Nature and Science")
-
-    expect(dmns.exhibits).to be_empty
-
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
-
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
-
-    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+      @dmns.add_exhibit(gems_and_minerals)
+      @dmns.add_exhibit(dead_sea_scrolls)
+      @dmns.add_exhibit(imax)
+      expect(@dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+    end
   end
 
-  it "has patrons" do
-    dmns = Museum.new("Denver Museum of Nature and Science")
+  describe '#admit(patron)' do
+    before :each do
+      @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      @imax = Exhibit.new({name: "IMAX",cost: 15})
 
-    expect(dmns.exhibits).to be_empty
+      @patron_1 = Patron.new("Bob", 0)
+      @patron_2 = Patron.new("Sally", 20)
+      @patron_3 = Patron.new("Johnny", 5)
+    end
 
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
+    it 'can add patrons to patron array' do
+      @dmns.admit(@patron_1)
+      @dmns.admit(@patron_2)
+      @dmns.admit(@patron_3)
 
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
+      expect(@dmns.patrons).to eq([@patron_1, @patron_2, @patron_3])
+    end
+  end
 
-    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
-    expect(dmns.patrons).to eq([])
+  describe '#add_exhibits' do
+    before :each do
+      @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      @imax = Exhibit.new({name: "IMAX",cost: 15})
+    end
 
-    patron_1 = Patron.new("Bob", 0)
-    patron_2 = Patron.new("Sally", 20)
-    patron_3 = Patron.new("Johnny", 5)
+    it 'can add exhibits to the museum' do
+      @dmns.add_exhibit(@gems_and_minerals)
+      @dmns.add_exhibit(@dead_sea_scrolls)
+      @dmns.add_exhibit(@imax)
 
+      expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+    end
+  end
+
+  describe '#recommend_exhibits(patron)' do
+    it 'can sort exhibits by patron interests' do
+      @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      @imax = Exhibit.new({name: "IMAX",cost: 15})
+
+      @dmns.add_exhibit(@gems_and_minerals)
+      @dmns.add_exhibit(@dead_sea_scrolls)
+      @dmns.add_exhibit(@imax)
+
+      @patron_1 = Patron.new("Bob", 20)
+
+      @patron_1.add_interest("Dead Sea Scrolls")      
+      @patron_1.add_interest("Gems and Minerals")
+
+      expect(@dmns.recommend_exhibits(@patron_1)).to be_a Array
+      expect(@dmns.recommend_exhibits(@patron_1)).to eq [@dead_sea_scrolls, @gems_and_minerals]
+    end
+  end
+
+  xit "has patrons" do
     patron_1.add_interest("Gems and Minerals")
     patron_1.add_interest("Dead Sea Scrolls")
     patron_2.add_interest("Dead Sea Scrolls")
     patron_3.add_interest("Dead Sea Scrolls")
-
-    dmns.admit(patron_1)
-    dmns.admit(patron_2)
-    dmns.admit(patron_3)
-
-    expect(dmns.patrons).to eq([patron_1,patron_2,patron_3])
-
   end
 
-  it "groups patrons by exhibit interest" do
-    dmns = Museum.new("Denver Museum of Nature and Science")
-
-    expect(dmns.exhibits).to be_empty
-
+  xit "groups patrons by exhibit interest" do
     gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     imax = Exhibit.new({name: "IMAX",cost: 15})
